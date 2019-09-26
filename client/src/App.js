9850b35fe9438b0af8c4d82af1770e326607ab30
Mohamed Gamal <mohamed.rady@fixedmea.com>
2019-09-26 10:39:53 +0200

    use blob method arrayBuffer instead of FileReader

--- client/src/App.js
+++ client/src/App.js
@@ -5,18 +5,14 @@ function App() {
   return (
     <div>
       <h1>Upload File</h1>
-      <input type="file" onChange={e => {
+      <input type="file" onChange={async (e) => {
         const file = e.target.files[0];
-        const reader = new FileReader();
-        reader.onload = async function (event) {
-          const buffer = event.target.result;
-          await axios.post('http://localhost:4040', buffer, {
-            headers: {
-              'Content-Type': 'application/octet-stream'
-            }
-          });
-        };
-        reader.readAsArrayBuffer(file);
+        const buffer = await file.arrayBuffer();
+        await axios.post('http://localhost:4040', buffer, {
+          headers: {
+            'Content-Type': 'application/octet-stream'
+          }
+        });
       }} />
     </div>
   );
